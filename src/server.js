00cb71a9d2b3ae8f4accb9b67968ff79c1241444
PixeL-rtx <zakroievdenis@icloud.com>
2025-03-03 20:01:14 +0100

    go_it_node.js

--- src/server.js
+++ src/server.js
@@ -1,55 +1,58 @@
-import express from 'express'
-import cors from 'cors'
-import pino from 'pino-http'
-import dotenv from 'dotenv'
-import { getEnvVar } from './utils/getEnvVar';
-import { getAllContacts, getContactById } from './services/contacts';
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import dotenv from 'dotenv';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 dotenv.config();
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
 export function setupServer() {
-    const app = express()
-    app.use(express.json())
-    app.use(cors())
-    app.use(
-        pino({
-            transport: {
-                target: 'pino-pretty',
-            },
-        })
-    )
-    app.get('/contacts', async (req, res) => {
-        const contact = await getAllContacts()
-        res.status(200).json({
-            data: contact,
-        });
-    });
-    app.get('/contacts/:contactsId', async (req, res, next) => {
-        const { contactId } = req.params;
-        const contact = await getContactById(contactId);
-        if (!contact) {
-            res.status(404).json({
-                message: 'Contact not found'
-            })
-            return
-        }
-        res.status(200).json({
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        });
-    });
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
+  app.get('/contacts', async (req, res) => {
+    const contact = await getAllContacts();
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contact,
+    });
+  });
 
-    app.use((req, res, next) => {
-        const url = req.url;
-        res.status(404).json({
-            status: 404,
-            message:'Route ${url} Not Found'
-        })
-   })
-    app.listen(PORT, () => {
-        console.log("Server is running on port {PORT}");
-    })
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    console.log(req.params);
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+    res.status(200).json({
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res, next) => {
+    const url = req.url;
+    res.status(404).json({
+      status: 404,
+      message: 'Route ${url} Not Found',
+    });
+  });
+  app.listen(PORT, () => {
+    console.log('Server is running on port {PORT}');
+  });
 }
--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -1,13 +1,10 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-
-
-dotenv.config()
-
+dotenv.config();
 
 export function getEnvVar(name, defaultValue) {
-    const value = process.getEnvVar[name]
-    if (value) return value;
-    if (defaultValue) return defaultValue;
-      throw new Error(`Missing: process.env['${name}'].`);
+  const value = process.env[name];
+  if (value) return value;
+  if (defaultValue) return defaultValue;
+  throw new Error(`Missing: process.env['${name}'].`);
 }

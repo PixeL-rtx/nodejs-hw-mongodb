29689da32930ac5b566875af4ca2e0f1e2264d5b
PixeL-rtx <zakroievdenis@icloud.com>
2025-03-27 08:59:38 +0100

    node._5_6

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -103,6 +103,6 @@ export const patchContactController = async (req, res, next) => {
   res.status(200).json({
     status: 200,
     message: `Successfully patched a contact!`,
-    data: result.value,
+    data: result,
   });
 };
